8d7231ceb0155fd93890ac881399ce734777897a
smootny <blazejewski@post.pl>
2025-02-08 22:41:05 +0100

    add deploy settings

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/blaze-sebastian",
+  // basePath: "/blaze-sebastian",
   images: {
     unoptimized: true,
   },
