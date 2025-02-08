e0c176b29e2d2be653ed2a26bab021db4bfe2a5e
smootny <blazejewski@post.pl>
2025-02-09 00:39:45 +0100

    add deploy settings

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
   basePath: "/blaze-sebastian",
+  assetPrefix: "/blaze-sebastian/",
   images: {
     unoptimized: true,
   },
