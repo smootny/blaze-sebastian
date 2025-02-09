b22d7d518eda56c1316f41e0f2327cf5ddf7c7d1
smootny <blazejewski@post.pl>
2025-02-09 01:12:36 +0100

    add deploy settings

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/blaze-sebastian",
+  basePath: "/blaze-sebastian", // Change to match your GitHub repo name
+  assetPrefix: "/blaze-sebastian/", // Ensure assets are loaded from the correct path
   images: {
-    unoptimized: true,
+    unoptimized: true, // Since GitHub Pages doesn't support Next.js image optimization
   },
 };
 
